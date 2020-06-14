c0f8aad0af62a5a126cfbcf7d3b86efd97e73d35
Javi Leyva <jvrleyva@gmail.com>
2020-06-14 18:48:49 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,6 +21,6 @@ class Doctor
   end
 
 def patients
-		self.appointments.collect { |appointments| appointment.patients }
+		self.appointments.collect { |appointment| appointments.patients }
 	end
 end 
