2fa0e4496ce566482b3d29916595ffd8bdd0dad2
Javi Leyva <jvrleyva@gmail.com>
2020-06-14 19:15:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,26 +1,27 @@
-class Doctor
-  attr_accessor :name
+class Doctor 
 
+  attr_accessor :name 
   @@all = []
 
   def initialize(name)
-    @name = name
+    @name = name 
     @@all << self
+  end 
+
+  def self.all 
+    @@all 
   end
 
-  def self.all
-    @@all
+  def appointments 
+    Appointment.all.filter { |appointment| appointment.doctor == self}
   end
 
-  def new_appointment(patient, date)
+  def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
 
-  def appointments
-    Appointment.all.select { |appointment| appointment.doctor == self }
+  def patients
+    appointments.map { |appointment| appointment.patient }
   end
 
- def patients
-    self.appointments.collect {|appointment| appointment.patient}
-  end
-end 
+end  
